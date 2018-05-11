b59000f6cc7d519b3fd7baecbe7de1fc6c4f01de
Peterson Salamat <petersonsalamat@gmail.com>
2018-05-11 19:28:50 +0800

    Started working on SoA Cell viewer

---
+++ RentLog.DomainLib45/SoaViewer/CellViewer/SoaCellViewerVM.cs
@@ -0,0 +1,20 @@
+﻿using CommonTools.Lib45.BaseViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentLog.DomainLib45.SoaViewer.CellViewer
+{
+    public class SoaCellViewerVM : MainWindowVMBase<AppArguments>
+    {
+        protected override string CaptionPrefix => "SoA Cell";
+
+
+        public SoaCellViewerVM(AppArguments appArguments) : base(appArguments)
+        {
+        }
+
+    }
+}
