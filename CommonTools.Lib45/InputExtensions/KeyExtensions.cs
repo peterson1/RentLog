d94606d25cc51c1508900c2623d1227f94833c1c
Peterson Salamat <petersonsalamat@gmail.com>
2018-07-02 10:20:58 +0800

    IsLetterOrDigit extension method for Input.Key

---
+++ CommonTools.Lib45/InputExtensions/KeyExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+
+namespace CommonTools.Lib45.InputExtensions
+{
+    public static class KeyExtensions
+    {
+        public static bool IsLetterOrDigit(this Key key)
+            => (key >= Key.A       && key <= Key.Z      ) 
+            || (key >= Key.D0      && key <= Key.D9     ) 
+            || (key >= Key.NumPad0 && key <= Key.NumPad9);
+    }
+}
