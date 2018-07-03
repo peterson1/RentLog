a364736fe3cedad4e1877095c826b00f10c3ade0
Peterson Salamat <petersonsalamat@gmail.com>
2018-07-04 05:41:01 +0800

    added Visible-if-null value converter

---
+++ CommonTools.Lib45/ValueConverters/VisibleIfNullConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CommonTools.Lib45.ValueConverters
+{
+    public class VisibleIfNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value == null ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
