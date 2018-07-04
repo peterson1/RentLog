7e05d7de11eaf480cdab4b83737d8abcfec0b0b9
Peterson Salamat <petersonsalamat@gmail.com>
2018-07-05 03:37:19 +0800

    Crud made public for testing

--- RentLog.Cashiering/BankDeposits/BankDepositsVM.cs
+++ RentLog.Cashiering/BankDeposits/BankDepositsVM.cs
@@ -8,18 +8,18 @@ namespace RentLog.Cashiering.BankDeposits
     [AddINotifyPropertyChangedInterface]
     public class BankDepositsVM : EncoderListVMBase<BankDepositDTO>
     {
-        private BankDepCrudVM _crud;
-
-
         public BankDepositsVM(MainWindowVM main) : base(main.ColxnsDB.BankDeposits, main)
         {
-            _crud = new BankDepCrudVM(main.ColxnsDB?.BankDeposits, main.AppArgs);
+            Crud = new BankDepCrudVM(main.ColxnsDB?.BankDeposits, main.AppArgs);
         }
 
 
+        public BankDepCrudVM Crud { get; }
+
+
         protected override string ListTitle    => "Bank Deposits";
-        protected override void   AddNewItem() => _crud.EncodeNewDraftCmd.ExecuteIfItCan();
+        protected override void   AddNewItem() => Crud.EncodeNewDraftCmd.ExecuteIfItCan();
         protected override Func<BankDepositDTO, decimal> SummedAmount => _ => _.Amount;
-        protected override void LoadRecordForEditing(BankDepositDTO rec) => _crud.EditCurrentRecord(rec);
+        protected override void LoadRecordForEditing(BankDepositDTO rec) => Crud.EditCurrentRecord(rec);
     }
 }
