fc757480a22adf3ff6e54f38e59a8a0854b2fdd3
Peterson Salamat <petersonsalamat@gmail.com>
2018-07-20 16:43:35 +0800

    Forward and Back buttons for Daily Status report

--- RentLog.DomainLib45/DailyStatusReporter/DailyStatusReportVM.cs
+++ RentLog.DomainLib45/DailyStatusReporter/DailyStatusReportVM.cs
@@ -1,4 +1,6 @@
-﻿using CommonTools.Lib45.BaseViewModels;
+﻿using CommonTools.Lib11.InputCommands;
+using CommonTools.Lib45.BaseViewModels;
+using CommonTools.Lib45.InputCommands;
 using RentLog.DomainLib11.DataSources;
 using RentLog.DomainLib11.Reporters;
 using System;
@@ -13,16 +15,24 @@ namespace RentLog.DomainLib45.DailyStatusReporter
 
         public DailyStatusReportVM(ITenantDBsDir dir) : base(dir)
         {
-            MinDate    = dir.Collections.AllDates().First();
-            MaxDate    = dir.Collections.LastPostedDate();
-            ReportDate = MaxDate;
+            MinDate         = dir.Collections.AllDates().First();
+            MaxDate         = dir.Collections.LastPostedDate();
+            ReportDate      = MaxDate;
+            PreviousDateCmd = R2Command.Relay(() => ReportDate = ReportDate.AddDays(-1),
+                                               _ => ReportDate > MinDate, "Previous Day");
+            NextDateCmd     = R2Command.Relay(() => ReportDate = ReportDate.AddDays(1),
+                                               _ => ReportDate < MaxDate, "Next Day");
         }
+
 
 
         public DateTime           MinDate     { get; }
         public DateTime           MaxDate     { get; }
         public DateTime           ReportDate  { get; set; }
         public DailyStatusReport  MainReport  { get; private set; }
+
+        public IR2Command   PreviousDateCmd  { get; }
+        public IR2Command   NextDateCmd      { get; }
 
 
         public void OnReportDateChanged() => ClickRefresh();
