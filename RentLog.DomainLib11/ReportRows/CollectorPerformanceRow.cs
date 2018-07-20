aace269b19ca10e055c06c297c1aa14894f8800f
Peterson Salamat <petersonsalamat@gmail.com>
2018-07-20 14:40:16 +0800

    Reads stall from current MarketState if snapshot is not available

--- RentLog.DomainLib11/ReportRows/CollectorPerformanceRow.cs
+++ RentLog.DomainLib11/ReportRows/CollectorPerformanceRow.cs
@@ -1,5 +1,6 @@
 ﻿using RentLog.DomainLib11.CollectionRepos;
 using RentLog.DomainLib11.DTOs;
+using RentLog.DomainLib11.MarketStateRepos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +9,12 @@ namespace RentLog.DomainLib11.ReportRows
 {
     public class CollectorPerformanceRow : List<CollectorPerfSubRow>
     {
-        public CollectorPerformanceRow(CollectorDTO collector, ICollectionsDB db)
+        public CollectorPerformanceRow(CollectorDTO collector, ICollectionsDB db, IStallsRepo fallbackStallsRepo)
         {
             Collector = collector;
 
             foreach (var sec in db.SectionsSnapshot)
-                AddSubRows(collector, sec, db);
+                AddSubRows(collector, sec, db, fallbackStallsRepo);
 
             Sections = this.GroupBy(_ => _.Section.Id)
                            .Select (_ => _.First())
@@ -33,14 +34,19 @@ namespace RentLog.DomainLib11.ReportRows
         //        => db.GetCollector(sec).Id == collector.Id).ToList();
 
 
-        private void AddSubRows(CollectorDTO collector, SectionDTO sec, ICollectionsDB db)
+        private void AddSubRows(CollectorDTO collector, SectionDTO sec, ICollectionsDB db, IStallsRepo stalls)
         {
             if (db.GetCollector(sec).Id != collector.Id) return;
             if (!db.IntendedColxns.TryGetValue(sec.Id, out IIntendedColxnsRepo repo)) return;
             if (!repo.Any()) return;
 
             foreach (var colxn in repo.GetAll())
+            {
+                if (colxn.StallSnapshot == null)
+                    colxn.StallSnapshot = stalls.Find(colxn.Lease.Stall.Id, true);
+
                 this.Add(new CollectorPerfSubRow(colxn, sec));
+            }
         }
     }
 }
--- RentLog.DomainLib11/Reporters/CollectorsPerformanceReport.cs
+++ RentLog.DomainLib11/Reporters/CollectorsPerformanceReport.cs
@@ -1,4 +1,5 @@
 ﻿using RentLog.DomainLib11.CollectionRepos;
+using RentLog.DomainLib11.MarketStateRepos;
 using RentLog.DomainLib11.ReportRows;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +8,10 @@ namespace RentLog.DomainLib11.Reporters
 {
     public class CollectorsPerformanceReport : List<CollectorPerformanceRow>
     {
-        public CollectorsPerformanceReport(ICollectionsDB db)
+        public CollectorsPerformanceReport(ICollectionsDB db, MarketStateDB mkt)
         {
             foreach (var collector in db.CollectorsSnapshot)
-                this.Add(new CollectorPerformanceRow(collector, db));
+                this.Add(new CollectorPerformanceRow(collector, db, mkt.Stalls));
 
             this.RemoveAll(_ => !_.Any());
         }
