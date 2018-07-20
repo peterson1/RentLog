ef3b71bdabb9af980c6bac31162df63501aa5e9a
Peterson Salamat <petersonsalamat@gmail.com>
2018-07-20 18:55:29 +0800

    Added PrintPreviewer to CommonTools

---
+++ CommonTools.Lib45/PrintTools/PrintPreviewWindow1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace CommonTools.Lib45.PrintTools
+{
+    public partial class PrintPreviewWindow1 : Window
+    {
+        public PrintPreviewWindow1()
+        {
+            InitializeComponent();
+            Loaded += (a, b) =>
+            {
+                var cc = vwr.Template.FindName("PART_FindToolBarHost", vwr) as ContentControl;
+                if (cc != null) cc.Visibility = Visibility.Collapsed;
+            };
+        }
+
+
+        public static void Show(IDocumentPaginatorSource documentPages, double inchesWidth, double inchesHeight, string printJobDesc)
+        {
+            var win = new PrintPreviewWindow1();
+            win.vwr.Document = documentPages;
+            win.Title = $"  Print Preview for “{printJobDesc}”  ({documentPages?.DocumentPaginator?.PageCount} sheets on {inchesWidth:N1} x {inchesHeight:N1} inches)";
+            win.Show();
+        }
+    }
+}
