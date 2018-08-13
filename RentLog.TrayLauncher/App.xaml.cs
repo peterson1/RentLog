9cc4f58382b3aa2e503f685f65e6c57f473da4e3
Peterson Salamat <petersonsalamat@gmail.com>
2018-08-13 13:33:33 +0800

    Added TrayLauncher proj and updated Nugets to their latest stable versions.

---
+++ RentLog.TrayLauncher/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System.Drawing;
+using System.Windows;
+
+namespace RentLog.TrayLauncher
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var tbi = new TaskbarIcon();
+            tbi.Icon = new Icon("Icons/strawberry_29.ico");
+            //tbi.IconSource = new BitmapImage(new Uri("Icons/strawberry_29.ico", UriKind.Relative));
+        }
+    }
+}
--- RentLog.TrayLauncher/Properties/AssemblyInfo.cs
+++ RentLog.TrayLauncher/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RentLog.LeasesCrud")]
+[assembly: AssemblyTitle("RentLog.TrayLauncher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RentLog.LeasesCrud")]
+[assembly: AssemblyProduct("RentLog.TrayLauncher")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.167.0532")]
-[assembly: AssemblyFileVersion("1.0.167.0532")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
