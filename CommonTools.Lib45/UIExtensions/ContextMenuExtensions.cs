1c6e89290be04a66e7ec1137d6b1ae0266ebdb76
Peterson Salamat <petersonsalamat@gmail.com>
2018-08-14 12:12:56 +0800

    ContextMenu tools

---
+++ CommonTools.Lib45/UIExtensions/ContextMenuExtensions.cs
@@ -0,0 +1,44 @@
+﻿using CommonTools.Lib11.InputCommands;
+using CommonTools.Lib45.InputCommands;
+using CommonTools.Lib45.TextLabels;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace CommonTools.Lib45.UIExtensions
+{
+    public static class ContextMenuExtensions
+    {
+        public static void AddLauncher(this ContextMenu menu, ExeLauncherCommand command)
+            => menu.Items.Add(NewLauncher(command));
+
+
+        public static void Add(this ContextMenu menu, string text)
+            => menu.Items.Add(new MenuItem
+            {
+                Header    = text,
+                IsEnabled = false
+            });
+
+
+        public static void Add(this ContextMenu menu, IR2Command command)
+        {
+            var mnu     = new MenuItem();
+            mnu.Command = command;
+            var bnd     = new Binding(nameof(IR2Command.CurrentLabel));
+            bnd.Source  = command;
+            BindingOperations.SetBinding(mnu, MenuItem.HeaderProperty, bnd);
+            menu.Items.Add(mnu);
+        }
+
+
+        private static MenuItem NewLauncher(ExeLauncherCommand launcherCmd)
+        {
+            var mnu         = new MenuItem();
+            mnu.DataContext = launcherCmd;
+            mnu.Header      = new MenuItemHeader1();
+            mnu.Command     = launcherCmd;
+            return mnu;
+        }
+    }
+}
