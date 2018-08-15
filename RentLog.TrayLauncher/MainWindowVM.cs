0a0d16733e0e54dd347acfc2a4fa832772a0d5d0
Peterson Salamat <petersonsalamat@gmail.com>
2018-08-15 17:44:58 +0800

    Launcher exits if invalid credentials

--- RentLog.TrayLauncher/MainWindowVM.cs
+++ RentLog.TrayLauncher/MainWindowVM.cs
@@ -3,9 +3,9 @@ using CommonTools.Lib11.StringTools;
 using CommonTools.Lib45.BaseViewModels;
 using CommonTools.Lib45.InputCommands;
 using CommonTools.Lib45.LicenseTools;
-using System;
+using CommonTools.Lib45.ThreadTools;
 using System.IO;
-using System.Windows.Input;
+using System.Threading.Tasks;
 using static RentLog.TrayLauncher.Properties.Settings;
 
 
@@ -19,18 +19,16 @@ namespace RentLog.TrayLauncher
 
         public MainWindowVM(LauncherArguments appArguments) : base(appArguments)
         {
-            if (TryParseCredentials())
-            {
-                EncoderCmd      = NewCmd("RL.Cashier.exe", "Encode Collections");
-                ReviewerCmd     = NewCmd("RL.Cashier.exe", "Review Collections");
-                StallsCmd       = NewCmd("RL.Stalls.exe" , "Stalls & Sections");
-                LeasesCmd       = NewCmd("RL.Leases.exe" , "Tenants & Leases");
-                ChequesCmd      = NewCmd("RL.Cheques.exe", "Vouchers & DCDR");
-                OverduesCmd     = NewCmd("RL.Reports.exe", "Backrentals & Overdue Rights", "Overdues");
-                ColxnSummaryCmd = NewCmd("RL.Reports.exe", "Collections Summary Report"  , "ColxnSmry");
-                DailyStatusCmd  = NewCmd("RL.Reports.exe", "Daily Status Report"         , "DailyStatus");
-            }
             UpdateNotifier.ExecuteOnFileChanged = true;
+            EncoderCmd      = NewCmd("RL.Cashier.exe", "Encode Collections");
+            ReviewerCmd     = NewCmd("RL.Cashier.exe", "Review Collections");
+            StallsCmd       = NewCmd("RL.Stalls.exe" , "Stalls & Sections");
+            LeasesCmd       = NewCmd("RL.Leases.exe" , "Tenants & Leases");
+            ChequesCmd      = NewCmd("RL.Cheques.exe", "Vouchers & DCDR");
+            OverduesCmd     = NewCmd("RL.Reports.exe", "Backrentals & Overdue Rights", "Overdues");
+            ColxnSummaryCmd = NewCmd("RL.Reports.exe", "Collections Summary Report"  , "ColxnSmry");
+            DailyStatusCmd  = NewCmd("RL.Reports.exe", "Daily Status Report"         , "DailyStatus");
+            ClickRefresh();
         }
 
 
@@ -39,21 +37,32 @@ namespace RentLog.TrayLauncher
 
         public ExeLauncherCommand   EncoderCmd       { get; }
         public ExeLauncherCommand   ReviewerCmd      { get; }
-                                                     
+
         public ExeLauncherCommand   StallsCmd        { get; }
         public ExeLauncherCommand   LeasesCmd        { get; }
         public ExeLauncherCommand   ChequesCmd       { get; }
-                                                     
+
         public ExeLauncherCommand   OverduesCmd      { get; }
         public ExeLauncherCommand   ColxnSummaryCmd  { get; }
         public ExeLauncherCommand   DailyStatusCmd   { get; }
-                                                     
+
         public string               NameAndRole      { get; private set; } = "verifying ...";
         public string               ArgumentError    { get; private set; }
 
 
-        private bool TryParseCredentials()
+        protected override async Task OnRefreshClickedAsync()
         {
+            if (!(await TryParseCredentials()))
+            {
+                Alert.ShowModal("Not Authorized", "Invalid credentials");
+                CloseWindow();
+            }
+        }
+
+
+        private async Task<bool> TryParseCredentials()
+        {
+            await Task.Delay(0);
             var ok = SeatLicenser.TryGetCredentials(AppArgs.CredentialsKey, 
                         out FirebaseCredentials creds, out string err);
             if (ok) NameAndRole = creds.NameAndRole;
