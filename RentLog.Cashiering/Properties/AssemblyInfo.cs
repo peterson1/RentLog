313e3eda9b4a292398231326e7d76d6e297657cd
Peterson Salamat <petersonsalamat@gmail.com>
2018-09-12 10:25:18 +0800

    Deployed ad hoc job for recomputing all lease balances since contract start.

--- RentLog.Cashiering/Properties/AssemblyInfo.cs
+++ RentLog.Cashiering/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.193.2235")]
-[assembly: AssemblyFileVersion("1.0.193.2235")]
+[assembly: AssemblyVersion("1.0.194.0222")]
+[assembly: AssemblyFileVersion("1.0.194.0222")]
