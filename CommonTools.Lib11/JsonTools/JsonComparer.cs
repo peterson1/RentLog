1d39c30a6f0109bfc0f1bd72da29dfc6b38b56cb
Peterson Salamat <petersonsalamat@gmail.com>
2018-10-02 14:55:00 +0800

    More properties.

--- CommonTools.Lib11/JsonTools/JsonComparer.cs
+++ CommonTools.Lib11/JsonTools/JsonComparer.cs
@@ -8,18 +8,25 @@ namespace CommonTools.Lib11.JsonTools
 {
     public class JsonComparer
     {
-        public JsonComparer(object document1, object document2)
+        public JsonComparer(object commonKey, object document1, object document2)
         {
+            CommonKey = commonKey;
             IsTheSame = SeeIfTheSame(Document1 = document1,
                                      Document2 = document2);
         }
 
 
+        public object  CommonKey    { get; }
         public object  Document1    { get; }
         public object  Document2    { get; }
         public bool?   IsTheSame    { get; }
+        public string  Json1        { get; private set; }
+        public string  Json2        { get; private set; }
         public string  CompareError { get; private set; }
         public string  Difference   { get; private set; }
+
+        public bool BothNull => Document1 == null
+                             && Document2 == null;
 
 
         private bool? SeeIfTheSame(object doc1, object doc2)
@@ -39,15 +46,17 @@ namespace CommonTools.Lib11.JsonTools
 
         private string GetDifference(object doc1, object doc2)
         {
-            var json1    = doc1.ToJson(true).SplitByLine();
-            var json2    = doc2.ToJson(true).SplitByLine();
-            var maxLines = Math.Max(json1.Length, json2.Length);
+            Json1        = doc1.ToJson(true);
+            Json2        = doc2.ToJson(true);
+            var jsLines1 = Json1.SplitByLine();
+            var jsLines2 = Json2.SplitByLine();
+            var maxLines = Math.Max(jsLines1.Length, jsLines2.Length);
             var diffs    = new List<string>();
 
             for (int i = 0; i < maxLines; i++)
             {
-                var line1 = json1.ElementAtOrDefault(i);
-                var line2 = json2.ElementAtOrDefault(i);
+                var line1 = jsLines1.ElementAtOrDefault(i);
+                var line2 = jsLines2.ElementAtOrDefault(i);
 
                 if (line1 != line2)
                     diffs.Add($"[{line1}]" + " - vs - " 
