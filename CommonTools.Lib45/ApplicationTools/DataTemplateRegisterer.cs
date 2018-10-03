8ec382fd1d2ea704d27ba6604b506be271854f37
Peterson Salamat <petersonsalamat@gmail.com>
2018-10-03 11:24:51 +0800

    Helper method for registering data templates at run-time

---
+++ CommonTools.Lib45/ApplicationTools/DataTemplateRegisterer.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace CommonTools.Lib45.ApplicationTools
+{
+    public static class DataTemplateRegisterer
+    {
+        public static void SetTemplate<TData, TUI>(this Application app)
+        {
+            var dict = app.Resources;
+            var key  = new DataTemplateKey(typeof(TData));
+            var elm  = new FrameworkElementFactory(typeof(TUI));
+            var tpl  = new DataTemplate { VisualTree = elm };
+            dict.Add(key, tpl);
+        }
+    }
+}
