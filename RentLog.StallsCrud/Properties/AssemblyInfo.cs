bf9984dcfb2f22f552afd9a89071367c6562e83a
Peterson Salamat <petersonsalamat@gmail.com>
2018-10-13 14:48:41 +0800

    Shows OtherCollection remarks as row tooltips

--- RentLog.StallsCrud/Properties/AssemblyInfo.cs
+++ RentLog.StallsCrud/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.226.0415")]
-[assembly: AssemblyFileVersion("1.0.226.0415")]
+[assembly: AssemblyVersion("1.0.227.0638")]
+[assembly: AssemblyFileVersion("1.0.227.0638")]
