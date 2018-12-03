30bb74ac44c905cae9692b89129c3e07fbd42645
Peterson Salamat <petersonsalamat@gmail.com>
2018-12-04 04:35:49 +0800

    Compact toolbar in cashier encoding screen

--- RentLog.StallsCrud/Properties/AssemblyInfo.cs
+++ RentLog.StallsCrud/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.267.1947")]
-[assembly: AssemblyFileVersion("1.0.267.1947")]
+[assembly: AssemblyVersion("1.0.268.2033")]
+[assembly: AssemblyFileVersion("1.0.268.2033")]
