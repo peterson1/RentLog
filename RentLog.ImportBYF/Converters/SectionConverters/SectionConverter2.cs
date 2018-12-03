5fc7c894032c21aa82b92ce8480497e78deac021
Peterson Salamat <petersonsalamat@gmail.com>
2018-12-04 03:33:04 +0800

    Added importer remedial method for setting stall templates for each section.

--- RentLog.ImportBYF/Converters/SectionConverters/SectionConverter2.cs
+++ RentLog.ImportBYF/Converters/SectionConverters/SectionConverter2.cs
@@ -1,7 +1,10 @@
 ﻿using CommonTools.Lib11.DynamicTools;
+using CommonTools.Lib11.InputCommands;
+using CommonTools.Lib45.InputCommands;
 using RentLog.DomainLib11.DataSources;
 using RentLog.DomainLib11.DTOs;
 using RentLog.DomainLib11.MarketStateRepos;
+using RentLog.DomainLib11.Models;
 using RentLog.ImportBYF.Version2UI;
 using RentLog.ImportBYF.Version2UI.MasterDataPane.ConvertersList;
 using System.Collections.Generic;
@@ -28,6 +31,37 @@ namespace RentLog.ImportBYF.Converters.SectionConverters
                 IsActive      = true,
                 StallTemplate = StallDTO.Named(nme + " {0:000}"),
             };
+        }
+
+
+        protected override IR2Command CreateRemediate1Cmd()
+            => R2Command.Relay(SetStallTemplates);
+
+
+        private void SetStallTemplates()
+        {
+            var repo = Main.AppArgs.MarketState.Sections;
+            foreach (var sec in repo.GetAll())
+            {
+                var tpl         = sec.StallTemplate;
+                tpl.Section     = sec;
+                tpl.DefaultRent = new RentParams
+                {
+                    Interval        = BillInterval.Daily,
+                    GracePeriodDays = 3,
+                    PenaltyRule     = "Daily Surcharge",
+                    PenaltyRate1    = 0.03M,
+                };
+                tpl.DefaultRights = new RightsParams
+                {
+                    SettlementDays = 180,
+                    PenaltyRule    = "Daily Surcharge after 90 days",
+                    PenaltyRate1   = 0.2M,
+                    PenaltyRate2   = 0.03M
+                };
+                tpl.IsOperational = true;
+                repo.Update(sec);
+            }
         }
 
 
