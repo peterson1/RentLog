04def955d647d63afa736aad29f067c4c824bbc6
Peterson Salamat <petersonsalamat@gmail.com>
2019-03-03 09:49:37 +0800

    Moved BalanceForwarder logic to Core

---
+++ RentLog.DomainLib11/BillingRules/BalanceForwarder.cs
@@ -0,0 +1,39 @@
+﻿using RentLog.DomainLib11.DataSources;
+using RentLog.DomainLib11.DTOs;
+using RentLog.DomainLib11.Models;
+
+namespace RentLog.DomainLib11.BillingRules
+{
+    public static class BalanceForwarder
+    {
+        public static void ForwardBalancesTo(this InactiveLeaseDTO inactv, LeaseDTO newLse, ITenantDBsDir dir)
+        {
+            var adjDate  = dir.Collections.UnclosedDate();
+            var newAdjs  = dir.Collections.For(adjDate).BalanceAdjs;
+            var oldAdjs = dir.Collections.For(inactv.DeactivatedDate).BalanceAdjs;
+            var oldBills = dir.Balances.GetRepo(inactv).Latest();
+            foreach (var billCode in BillCodes.Collected())
+            {
+                var oldBal = oldBills.For(billCode);
+                if ((oldBal.ClosingBalance ?? 0) == 0) continue;
+                newAdjs.Insert(new BalanceAdjustmentDTO
+                {
+                    AmountOffset = oldBal.ClosingBalance.Value,
+                    BillCode     = billCode,
+                    DocumentRef  = "-",
+                    LeaseId      = newLse.Id,
+                    Reason       = $"Forwarded Balance from [{inactv.Id}]",
+                });
+
+                oldAdjs.Insert(new BalanceAdjustmentDTO
+                {
+                    AmountOffset = oldBal.ClosingBalance.Value * -1,
+                    BillCode     = billCode,
+                    DocumentRef  = "-",
+                    LeaseId      = inactv.Id,
+                    Reason       = $"Forwarded Balance to [{newLse.Id}]",
+                });
+            }
+        }
+    }
+}
--- RentLog.FilteredLeases/LeaseCRUDs/LeaseCRUD1/LeaseCRUD1VM_Inactives.cs
+++ RentLog.FilteredLeases/LeaseCRUDs/LeaseCRUD1/LeaseCRUD1VM_Inactives.cs
@@ -2,9 +2,8 @@
 using CommonTools.Lib11.InputCommands;
 using CommonTools.Lib45.ThreadTools;
 using RentLog.DomainLib11.Authorization;
-using RentLog.DomainLib11.DataSources;
+using RentLog.DomainLib11.BillingRules;
 using RentLog.DomainLib11.DTOs;
-using RentLog.DomainLib11.Models;
 using RentLog.FilteredLeases.FilteredLists;
 
 namespace RentLog.FilteredLeases.LeaseCRUDs.LeaseCRUD1
@@ -35,40 +34,9 @@ namespace RentLog.FilteredLeases.LeaseCRUDs.LeaseCRUD1
                     crud.SetPickedStart(inactv.DeactivatedDate.AddDays(1).Date);
                     crud.SetRenewedFrom(inactv);
                     crud.SetProductToSell(inactv.ProductToSell);
-                    crud.NewRecordSaved += (s, newLse) => ForwardBalances(inactv, newLse, args);
+                    crud.NewRecordSaved += (s, newLse) => inactv.ForwardBalancesTo(newLse, args);
                     crud.EncodeNewDraftCmd.ExecuteIfItCan();
                 });
-        }
-
-
-        private static void ForwardBalances(InactiveLeaseDTO inactv, LeaseDTO newLse, ITenantDBsDir dir)
-        {
-            var adjDate  = dir.Collections.UnclosedDate();
-            var newAdjs  = dir.Collections.For(adjDate).BalanceAdjs;
-            var oldAdjs = dir.Collections.For(inactv.DeactivatedDate).BalanceAdjs;
-            var oldBills = dir.Balances.GetRepo(inactv).Latest();
-            foreach (var billCode in BillCodes.Collected())
-            {
-                var oldBal = oldBills.For(billCode);
-                if ((oldBal.ClosingBalance ?? 0) == 0) continue;
-                newAdjs.Insert(new BalanceAdjustmentDTO
-                {
-                    AmountOffset = oldBal.ClosingBalance.Value,
-                    BillCode     = billCode,
-                    DocumentRef  = "-",
-                    LeaseId      = newLse.Id,
-                    Reason       = $"Forwarded Balance from [{inactv.Id}]",
-                });
-
-                oldAdjs.Insert(new BalanceAdjustmentDTO
-                {
-                    AmountOffset = oldBal.ClosingBalance.Value * -1,
-                    BillCode     = billCode,
-                    DocumentRef  = "-",
-                    LeaseId      = inactv.Id,
-                    Reason       = $"Forwarded Balance to [{newLse.Id}]",
-                });
-            }
         }
     }
 }
